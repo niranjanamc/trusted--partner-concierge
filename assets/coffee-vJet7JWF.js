370460e87a836f27d8441335e240f4823180304b
Murthy <niranjana.mc@gmail.com>
2026-03-20 15:06:34 +0530

    Updates

--- assets/coffee-vJet7JWF.js
+++ assets/coffee-vJet7JWF.js
@@ -1,4 +1,4 @@
-import{c}from"./index-BrlEZVbf.js";/**
+import{c}from"./index-DWw-boZr.js";/**
  * @license lucide-react v0.556.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/send-D2wj-o0N.js
+++ assets/send-D2wj-o0N.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BrlEZVbf.js";/**
+import{c as a}from"./index-DWw-boZr.js";/**
  * @license lucide-react v0.556.0 - ISC
  *
  * This source code is licensed under the ISC license.
