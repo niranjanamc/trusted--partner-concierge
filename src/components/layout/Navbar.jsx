6a259ee8841380a57c5160a274fdf093f4e9be5a
Murthy <niranjana.mc@gmail.com>
2026-04-25 17:00:07 +0530

    Add icons to navigation menu items

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Menu, X } from 'lucide-react';
+import { Menu, X, Home, Plane, Building2, Car, Briefcase, ShieldCheck, Info, CalendarDays } from 'lucide-react';
 import styles from './Navbar.module.css';
 import logoImage from '../../assets/images/voyage_monk_logo_transparent.png';
 
@@ -31,17 +31,17 @@ const Navbar = () => {
 
                 {/* Desktop Menu */}
                 <div className={styles.desktopMenu}>
-                    <Link to="/" className={`${styles.navLink} ${location.pathname === '/' ? styles.activeLink : ''}`}>Home</Link>
-                    <Link to="/flight-booking" className={`${styles.navLink} ${location.pathname === '/flight-booking' ? styles.activeLink : ''}`}>Flights</Link>
-                    <Link to="/hotels" className={`${styles.navLink} ${location.pathname === '/hotels' ? styles.activeLink : ''}`}>Hotels</Link>
-                    <Link to="/car-rental" className={`${styles.navLink} ${location.pathname === '/car-rental' ? styles.activeLink : ''}`}>Car Rental</Link>
-                    <Link to="/corporate-events" className={`${styles.navLink} ${location.pathname === '/corporate-events' ? styles.activeLink : ''}`}>MICE</Link>
+                    <Link to="/" className={`${styles.navLink} ${location.pathname === '/' ? styles.activeLink : ''}`}><Home size={16} /> Home</Link>
+                    <Link to="/flight-booking" className={`${styles.navLink} ${location.pathname === '/flight-booking' ? styles.activeLink : ''}`}><Plane size={16} /> Flights</Link>
+                    <Link to="/hotels" className={`${styles.navLink} ${location.pathname === '/hotels' ? styles.activeLink : ''}`}><Building2 size={16} /> Hotels</Link>
+                    <Link to="/car-rental" className={`${styles.navLink} ${location.pathname === '/car-rental' ? styles.activeLink : ''}`}><Car size={16} /> Car Rental</Link>
+                    <Link to="/corporate-events" className={`${styles.navLink} ${location.pathname === '/corporate-events' ? styles.activeLink : ''}`}><Briefcase size={16} /> MICE</Link>
                     {/* DEPRECATED: Destinations page temporarily disabled
                     <Link to="/destinations" className={`${styles.navLink} ${location.pathname === '/destinations' ? styles.activeLink : ''}`}>Destinations</Link>
                     */}
-                    <Link to="/promise" className={`${styles.navLink} ${location.pathname === '/promise' ? styles.activeLink : ''}`}>The Promise</Link>
-                    <Link to="/about" className={`${styles.navLink} ${location.pathname === '/about' ? styles.activeLink : ''}`}>About Us</Link>
-                    <Link to="/contact" className={`${styles.navLink} ${styles.cta} ${location.pathname === '/contact' ? styles.activeLink : ''}`}>Plan My Trip</Link>
+                    <Link to="/promise" className={`${styles.navLink} ${location.pathname === '/promise' ? styles.activeLink : ''}`}><ShieldCheck size={16} /> The Promise</Link>
+                    <Link to="/about" className={`${styles.navLink} ${location.pathname === '/about' ? styles.activeLink : ''}`}><Info size={16} /> About Us</Link>
+                    <Link to="/contact" className={`${styles.navLink} ${styles.cta} ${location.pathname === '/contact' ? styles.activeLink : ''}`}><CalendarDays size={16} /> Plan My Trip</Link>
                 </div>
 
                 {/* Mobile Menu Button */}
@@ -56,17 +56,17 @@ const Navbar = () => {
                 {/* Mobile Menu */}
                 {isOpen && (
                     <div className={styles.mobileMenu}>
-                        <Link to="/" className={styles.mobileLink}>Home</Link>
-                        <Link to="/flight-booking" className={styles.mobileLink} onClick={() => setIsOpen(false)}>Flights</Link>
-                        <Link to="/hotels" className={styles.mobileLink} onClick={() => setIsOpen(false)}>Hotels</Link>
-                        <Link to="/car-rental" className={styles.mobileLink} onClick={() => setIsOpen(false)}>Car Rental</Link>
-                        <Link to="/corporate-events" className={styles.mobileLink} onClick={() => setIsOpen(false)}>MICE</Link>
+                        <Link to="/" className={styles.mobileLink}><Home size={18} /> Home</Link>
+                        <Link to="/flight-booking" className={styles.mobileLink} onClick={() => setIsOpen(false)}><Plane size={18} /> Flights</Link>
+                        <Link to="/hotels" className={styles.mobileLink} onClick={() => setIsOpen(false)}><Building2 size={18} /> Hotels</Link>
+                        <Link to="/car-rental" className={styles.mobileLink} onClick={() => setIsOpen(false)}><Car size={18} /> Car Rental</Link>
+                        <Link to="/corporate-events" className={styles.mobileLink} onClick={() => setIsOpen(false)}><Briefcase size={18} /> MICE</Link>
                         {/* DEPRECATED: Destinations page temporarily disabled
                         <Link to="/destinations" className={styles.mobileLink} onClick={() => setIsOpen(false)}>Destinations</Link>
                         */}
-                        <Link to="/promise" className={styles.mobileLink} onClick={() => setIsOpen(false)}>The Promise</Link>
-                        <Link to="/about" className={styles.mobileLink} onClick={() => setIsOpen(false)}>About Us</Link>
-                        <Link to="/contact" className={`${styles.mobileLink} ${styles.mobileCta}`}>Plan My Trip</Link>
+                        <Link to="/promise" className={styles.mobileLink} onClick={() => setIsOpen(false)}><ShieldCheck size={18} /> The Promise</Link>
+                        <Link to="/about" className={styles.mobileLink} onClick={() => setIsOpen(false)}><Info size={18} /> About Us</Link>
+                        <Link to="/contact" className={`${styles.mobileLink} ${styles.mobileCta}`}><CalendarDays size={18} /> Plan My Trip</Link>
                     </div>
                 )}
             </div>
